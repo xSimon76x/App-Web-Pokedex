5466c84519f483ff88dc7330b061bfcedf363de4
SimonIvan <73963276+xSimon76x@users.noreply.github.com>
2022-03-04 23:40:59 -0300

    favorite function completed

--- src/components/filterPokemonCard.js
+++ src/components/filterPokemonCard.js
@@ -1,86 +1,66 @@
 import ImgCard from "./imgCard.js";
 import { Link } from "react-router-dom";
 import "../assets/css/pokemonCard.css";
-import React, { useEffect, useState } from "react";
+import React from "react";
+
+
+
+let isFavorite = []
+function FilterPokemonCard(props) {
+
+    const { objPokemon, btnFavorite } = props;
 
 
 
 
-export default function FilterPokemonCard(props) {
+    return (
+        <div>
+            <div className="flexOrder">
 
-
-    const { objPokemon } = props
-    const [objetoPokemon, setObjetoPokemon] = useState(objPokemon)
-    const [favorite, setFavorite] = useState([])
-    const redHeart = "❤️"
-    const whiteHeart = "🤍"
-    let isFavorite = []
-
-    console.log(props)
-
-    const btnFavorite = (id, namePokemon, isFavorite) => {
-        let content = document.getElementById(id).textContent
-        let ind = 0
-        if (content == "🤍") {
-            document.getElementById(id).textContent = "❤️"
-            isFavorite.push({ id: id, name: namePokemon })
-            console.log(isFavorite)
-        } else {
-            document.getElementById(id).textContent = "🤍"
-            isFavorite.map((ct, index) => (
-                ((isFavorite[index].name).includes(namePokemon) ? (
-                    isFavorite.splice(index, 1),
-                    console.log(isFavorite)
-                ) : (
-                    false
-                ))
-            ))
-        }
-    }
-
-    if (favorite.name) {
-        console.log(favorite)
-    }
-
-    return (<div className="flexOrder">
-        {objPokemon ? (
-            objPokemon.map((ct, index) => (
-                <div key={index} className="d-flex justify-content-center">
-                    {objPokemon ? (
-                        <div className="pokeCard">
-                            <div className="containerImageHeart">
-                                <p id={"icoN" + objPokemon[index].name} onClick={(() => btnFavorite("icoN" + objPokemon[index].name, objPokemon[index].name, isFavorite))}>{whiteHeart}</p>
-                            </div>
-                            <Link
-                                to={"/pokedex/" + objPokemon[index].name}
-                                style={{ textDecoration: "none" }}
-                            >
-                                {/* <h3>#{counter++}</h3> */}
-                                <ImgCard namePokemon={objPokemon[index].name} />
-                                <h3>{objPokemon[index].name}</h3>
-                            </Link>
-                        </div>
-
-                    ) : (
-                        <>
-                            <div class="d-flex justify-content-center">
-                                <div class="spinner-border" role="status">
-                                    <span class="visually-hidden">Loading...</span>
+                {objPokemon ? (
+                    objPokemon.map((ct, index) => (
+                        <div key={index} className="d-flex justify-content-center">
+                            {objPokemon ? (
+                                <div className="pokeCard">
+                                    <div className="containerImageHeart">
+                                        <p id={"icoN" + objPokemon[index].name} onClick={(() => (btnFavorite("icoN" + objPokemon[index].name, objPokemon[index].name, isFavorite)))}>🤍</p>
+                                    </div>
+                                    <Link
+                                        to={"/pokedex/" + objPokemon[index].name}
+                                        style={{ textDecoration: "none" }}
+                                    >
+                                        {/* <h3>#{counter++}</h3> */}
+                                        <ImgCard namePokemon={objPokemon[index].name} />
+                                        <h3>{objPokemon[index].name}</h3>
+                                    </Link>
                                 </div>
-                            </div>
-                        </>
-                    )}
-                </div>
-            ))
-        ) : (
-            <div className="d-flex justify-content-center">
-                <div className="spinner-border" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </div>
+
+                            ) : (
+                                <>
+                                    <div class="d-flex justify-content-center">
+                                        <div class="spinner-border" role="status">
+                                            <span class="visually-hidden">Loading...</span>
+                                        </div>
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                    ))
+                ) : (
+                    <div className="d-flex justify-content-center">
+                        <div className="spinner-border" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                )}
             </div>
-        )}
-    </div>
+        </div>
+
+
 
     );
 
 };
+
+
+export default FilterPokemonCard;
--- src/components/informationCard.js
+++ src/components/informationCard.js
@@ -11,7 +11,8 @@ export default function InformationCard(props) {
   const { objPokemon, routeAPI, counterMaxPokemons } = props;
   const allPokemons = PokeApi(routeAPI + "?limit=" + 600)
   const [currentPokemon, setCurrentPokemon] = useState({ state: false, data: null })
-
+  const [listFavoritePokemon, setListFavoritePokemon] = useState(null);
+  const [btnPressFavorites, setBtnPressFavorites] = useState(false)
 
 
   const searchName = (objPokemon) => {
@@ -36,24 +37,58 @@ export default function InformationCard(props) {
     }
   }
 
+  function btnFavorite(id, namePokemon, isFavorite) {
+    let content = document.getElementById(id).textContent;
+
+    if (content == "🤍") {
+      document.getElementById(id).textContent = "❤️"
+      isFavorite.push({ id: id, name: namePokemon })
+      return setListFavoritePokemon(isFavorite);
+    } else {
+      document.getElementById(id).textContent = "🤍"
+      isFavorite.map((ct, index) => (
+        ((isFavorite[index].name).includes(namePokemon) ? (
+          isFavorite.splice(index, 1)
+        ) : (
+          false
+        ))
+      ))
+      return setListFavoritePokemon(isFavorite)
+    }
+  }
+
+
+
+
 
   return (
     <div>
       <div className="d-flex bd-highlight py-4 px-5">
         <div className="p-2 w-100 bd-highlight"><input id="ip-Search" type="text" className="form-control" placeholder="Search your pokemon" onChange={(() => searchName(allPokemons))}></input></div>
         <div className="d-flex p-2 flex-shrink-1 bd-highlight align-content-center">
-          <button type="button" className="btn btn-primary">
+          <button type="button" className="btn btn-primary" onClick={(() => setBtnPressFavorites(true))}>
             <img src={heart} style={{ width: "30px", height: "25px" }}></img>
           </button>
         </div>
       </div>
       <div>
         {objPokemon && !currentPokemon.data ? (
-
-          <FilterPokemonCard objPokemon={objPokemon.results} routeAPI={routeAPI} />
+          btnPressFavorites == true ? (
+            <FilterPokemonCard objPokemon={listFavoritePokemon} routeAPI={routeAPI} btnFavorite={btnFavorite} />
+          ) : (
+            <FilterPokemonCard objPokemon={objPokemon.results} routeAPI={routeAPI} btnFavorite={btnFavorite} />
+          )
         ) : (
 
-          <FilterPokemonCard objPokemon={currentPokemon.data} routeAPI={routeAPI} />
+          !objPokemon ? (
+            <div className="d-flex justify-content-center">
+              <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
+            </div>
+          ) : (
+            <FilterPokemonCard objPokemon={currentPokemon.data} routeAPI={routeAPI} btnFavorite={btnFavorite} />
+          )
         )}
       </div>
     </div >
